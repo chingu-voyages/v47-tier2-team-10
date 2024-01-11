4f76c2ec7492f3e2b8dcd08366c3f35e0235d454
anthony <anthonycakins@gmail.com>
2024-01-11 15:16:12 +1000

    testing

---
+++ v47-client/src/App.jsx
@@ -0,0 +1,15 @@
+import {browserRouter as Router, Routes, Route} from "react-dom"
+import Landing from "./pages/Landing"
+import ProductPage from "./pages/ProductPage"
+export default function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="" element={<Landing />} />
+          <Route path="/ProductPage" element={<ProductPage />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
