74bff2bfae6c5cc189529b53eb1d7c23c74540c7
SkylarPark <80894789+MinjuSkylarPark@users.noreply.github.com>
2024-01-13 15:31:50 +0900

    created temp footer

--- v47-client/src/pages/Footer.jsx
+++ v47-client/src/pages/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='site-footer bg-gray-800 p-10 text-base leading-6 text-gray-500 flex flex-col'>
+    <footer className='site-footer bg-gray-800 p-10 text-base leading-6 text-blue-300 flex flex-col'>
       {/* Top Section */}
       <div className="grid grid-cols-3 gap-4">
         {/* Team Members Section */}
@@ -10,29 +10,31 @@ const Footer = () => {
           <p className=' font-bold text-xl'>Team members</p>
             <div className='mt-1'>
               <ul className="footer-links list-none">
-                <li><a href="https://github.com/MinjuSkylarPark" className="text-gray-500">Skylar github.com/MinjuSkylarPark</a></li>
-                <li><a href="https://github.com/Cakinn1" className="text-gray-500">Anthony github.com/Cakinn1</a></li>
-                <li><a href="https://github.com/devavaz" className="text-gray-500">Avaz github.com/devavaz</a></li>
-                <li><a href="https://github.com/Choiz0" className="text-gray-500">Jiyoung github.com/Choiz0</a></li>
-                <li><a href="https://github.com/jessabc" className="text-gray-500">Jess github.com/jessabc</a></li>
-                <li><a href="#" className="text-gray-500">Lili github.com/Lili</a></li>
+                <li><a href="https://github.com/MinjuSkylarPark" className="text-blue-300">Skylar github.com/MinjuSkylarPark</a></li>
+                <li><a href="https://github.com/Cakinn1" className="text-blue-300">Anthony github.com/Cakinn1</a></li>
+                <li><a href="https://github.com/devavaz" className="text-blue-300">Avaz github.com/devavaz</a></li>
+                <li><a href="https://github.com/Choiz0" className="text-blue-300">Jiyoung github.com/Choiz0</a></li>
+                <li><a href="https://github.com/jessabc" className="text-blue-300">Jess github.com/jessabc</a></li>
+                <li><a href="#" className="text-blue-300">Lili github.com/Lili</a></li>
               </ul>
           </div>
         </div>
         {/* About Us Section */}
         <div className='container-none text-center '>
           <p className=' font-bold text-xl'>About us</p>
-          <p>v47 Team 10</p>
+          <p><a href="https://github.com/MinjuSkylarPark/v47-tier2-team-10" className='text-blue-300'>v47 Team 10</a></p>
         </div>
         {/* Copyright Section */}
-        <div className='container-none'>
-          <p className="copyright-text text-center md:text-left mt-0 md:mt-0 sm:mt-0">
-            <p className='text-xl'>Copyright &copy; <br /></p>
+        <div className='container-none '>
+          <p className="copyright-text md:text-left mt-0 md:mt-0 sm:mt-0">
+            <p className='text-xl text-center'>Copyright &copy; <br /></p>
+            <p className='text-center'> Voyage team 10 2024</p>
           </p>
-          <a href="https://github.com/MinjuSkylarPark/v47-tier2-team-10" className="text-blue-500"> Voyage team 10 2024</a>.
+     
             {/* Bottom Section (Terms and Policies) */}
-          <div className="container-none mt-20 ">
-            <p className='font-bold text-xl'>terms and policy</p>
+          <div className="container-none mt-20 text-center">
+            <br/>
+            <p className='font-bold text-xl'><a href="https://www.forbes.com/advisor/business/why-your-website-needs-terms-of-use-agreement/" className='text-blue-300'>terms and policy</a></p>
             {/* Empty for now, add any additional sections if needed */}
           </div>
         </div>
