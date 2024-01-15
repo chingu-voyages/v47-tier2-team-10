e003abaf1255acb459d9dbdb55ae537e2cb218dc
jess <120062852+jessabc@users.noreply.github.com>
2024-01-15 14:14:31 +1100

    implement activity

---
+++ v47-client/src/components/productpage/Activity.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { GoTasklist } from "react-icons/go";
+import { MdDeleteForever } from "react-icons/md";
+
+
+export default function Activity({activity}) {
+
+    return (
+    <div className='md:flex items-center gap-1 ml-10 hidden'>
+        <button className=' '>
+            <GoTasklist /> 
+        </button>
+        <p>{activity.activityName}</p>
+        <button className=' '>
+            <MdDeleteForever />
+        </button>
+    </div>
+  )
+}
