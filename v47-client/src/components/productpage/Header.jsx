53c7b197a429013ea052b2d15423d4f14833cbaf
jess <120062852+jessabc@users.noreply.github.com>
2024-01-15 14:13:48 +1100

    add hamburger

--- v47-client/src/components/productpage/Header.jsx
+++ v47-client/src/components/productpage/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 
-export default function Header() {
+
+export default function Header({isLeftNavOpen, setIsLeftNavOpen}) {
   return (
-    <section className='border  p-6'>
-      Header
+    <section className='border  p-6 flex justify-between items-center'>
+      <p>Header</p>
+
+      {<button onClick={() => setIsLeftNavOpen(prev => !prev)} className='mb-auto  md:hidden'>
+        {!isLeftNavOpen && <GiHamburgerMenu />}
+        {isLeftNavOpen && <IoClose />}
+      </button>}
     </section>
   )
 }
