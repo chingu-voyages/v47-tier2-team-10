a6fc303cb89eca65553cc572f5072eab09b8bb76
jess <120062852+jessabc@users.noreply.github.com>
2024-01-20 13:54:53 +1100

    change icons, tailwind

--- v47-client/src/components/productpage/LeftNav/Category.jsx
+++ v47-client/src/components/productpage/LeftNav/Category.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { TbCategoryPlus } from "react-icons/tb";
-import { TbCategoryMinus } from "react-icons/tb";
+import { MdExpandMore } from "react-icons/md";
+import { MdExpandLess } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import Activity from "./Activity";
-import { TbCategory } from "react-icons/tb";
+import { GrAddCircle } from "react-icons/gr";
 
 export default function Category({ category, handleFilterData}) {
   const [isActivityVisible, setIsActivityVisible] = useState(false);
@@ -26,32 +26,36 @@ export default function Category({ category, handleFilterData}) {
   return (
     <>
 
-      <div className="flex justify-between items-center gap-1 md:mt-3 mb-1">
-        <div className="flex gap-1  hover:text-gray-700 font-medium">
+      <div className="flex justify-between items-center gap-1 mt-3 md:mt-5">
+        <div className="flex gap-1  md:hover:text-gray-700 font-medium">
           <div className="hidden md:block hover:text-gray-700 font-medium text-lg">
             <button onClick={() => setIsActivityVisible((prev) => !prev)}>
-              {isActivityVisible ? <TbCategoryMinus /> : <TbCategoryPlus />}
+              {isActivityVisible ? <MdExpandLess /> : <MdExpandMore />}
             </button>
           </div>
           <button
             onClick={() => setIsActivityVisible((prev) => !prev)}
-            className="flex justify-center items-center gap-2 "
+            className="flex justify-center items-center gap-2"
           >
-            <div className="md:hidden">
-              <TbCategory />
-            </div>
-            <div className="hover:text-gray-700">
+            <div className="text-gray-400 md:text-gray-900 md:hover:text-gray-700">
               {capitalizeEachWord(category.categoryName)}
             </div>
           </button>
         </div>
-        <button className="hidden md:block font-bold text-xl text-red-500 hover:text-red-400">
-          <MdDeleteOutline />
-        </button>
+        <div className='flex gap-1'>
+          <button className='hidden md:block font-bold text-xl text-gray-900 hover:text-gray-700'>
+            <GrAddCircle />
+          </button>
+          <button className="hidden md:block font-bold text-xl text-red-500 hover:text-red-400">
+            <MdDeleteOutline />
+          </button>
+        </div>
       </div>
 
 
-      {isActivityVisible && <div>{activityEl}</div>}
+      <div className={`${isActivityVisible ? 'block' : 'block md:hidden'}`}>
+        {activityEl}
+      </div>
     </>
   );
 }
