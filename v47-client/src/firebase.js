a53de0c71b06e32357a41b60a316f6599f6ce6a5
SkylarPark <80894789+MinjuSkylarPark@users.noreply.github.com>
2024-02-11 17:56:12 +0900

    to check blockers - for personal use

--- v47-client/src/firebase.js
+++ v47-client/src/firebase.js
@@ -3,24 +3,15 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_MYFIRE_DATABASE_,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-// };
-
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+
+  // apiKey:process.env.REACT_APP_MYFIRE_DATABASE,
+  // blocker: It works only when actual apiKey exists here.
   apiKey:"AIzaSyASpMTTkiFYeFb0DlvW4JieANVRYgx4J5o",
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId:"v47login",
+  projectId:process.env.REACT_APP_PROJECT_ID,
   storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
