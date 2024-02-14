438535f3c32b15f26c9f18af3d6ae38108b5d320
jess <120062852+jessabc@users.noreply.github.com>
2024-02-14 15:56:08 +1100

    delete category and activity logic

--- v47-client/src/components/productpage/modals/Delete.jsx
+++ v47-client/src/components/productpage/modals/Delete.jsx
@@ -2,12 +2,12 @@
 import React from 'react';
 import Portal from "./Portal/Portal"
 
-export const Delete = ({ onDelete, onCancel }) => {
+export const Delete = ({ onDelete, onCancel, name }) => {
   return (
     <Portal>
     <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 bg-gray-800 z-50">
       <div className="bg-white p-8 rounded-lg">
-        <p className="text-gray-700 text-lg mb-10 mx-20">Are you sure you want to delete?</p>
+      <p className="text-gray-700 text-lg mb-10 mx-20">{`Are you sure you want to delete '${name}'?`}</p>
         <div className="flex justify-evenly">
           <button
             className="bg-red-500 text-white px-8 py-2 mr-2 rounded"
