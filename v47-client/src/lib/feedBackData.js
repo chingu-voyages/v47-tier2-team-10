b35ecedcbcf1bf1561114e4f257ef0a9505d49e0
devavaz <135300758+devavaz@users.noreply.github.com>
2024-02-15 20:36:18 +0900

    updated TeamInfo comp

--- v47-client/src/lib/feedBackData.js
+++ v47-client/src/lib/feedBackData.js
@@ -1,4 +1,4 @@
-import { people01, people02, people03, github, linkedin } from "../assets";
+import { people01, people02, people03, github, linkedin, avazImg, cakinImg, skylarImg, jessImg } from "../assets";
 
 export const feedback = [
   {
@@ -34,44 +34,54 @@ export const TeamData = [
     id: "feedback-1",
     content:
       "Your platform has revolutionized the way I manage tasks and stay organized. It's not just a tool; it's my personal productivity companion.",
-    name: "Sarah Thompson",
+    name: "Anthony Cakin", 
     title: "Frontend Developer",
-    img: people01,
+    img: cakinImg,
     flag: "🇺🇸",
     iconGithub: github,
     iconLinkedin: linkedin,
+    githubLink: "https://github.com/Cakinn1",
+    linkedinLink: "https://www.linkedin.com/in/anthony-cakin/"
+
   },
   {
     id: "feedback-2",
     content:
       "I've tried various productivity apps, but yours stands out. It simplifies my workflow, making everyday tasks a breeze to manage.",
-    name: "John Anderson",
+    name: "Skylar Park",
     title: "Frontend Developer",
-    img: people02,
+    img: skylarImg,
     flag: "🇨🇦",
     iconGithub: github,
     iconLinkedin: linkedin,
+    githubLink: "https://github.com/MinjuSkylarPark",
+    linkedinLink: "https://www.linkedin.com/in/skylar-park-a407aa22a/"
   },
   {
     id: "feedback-3",
     content:
       "Being organized has never been this easy. Your platform has become an integral part of my daily routine, helping me achieve more with less effort.",
-    name: "Emily Rodriguez",
+    name: "Avazbek",
     title: "Frontend Developer",
-    img: people03,
+    img: avazImg,
     flag: "🇬🇧",
     iconGithub: github,
     iconLinkedin: linkedin,
+    githubLink: "https://github.com/devavaz",
+    linkedinLink: "https://www.linkedin.com/in/avazbeks/"
+
   },
   {
     id: "feedback-3",
     content:
       "Being organized has never been this easy. Your platform has become an integral part of my daily routine, helping me achieve more with less effort.",
-    name: "Emily Rodriguez",
+    name: "Jess",
     title: "Frontend Developer",
-    img: people03,
-    flag: "🇬🇧",
+    img: jessImg,
+    flag: "🇺🇸",
     iconGithub: github,
     iconLinkedin: linkedin,
+    githubLink: "https://github.com/jessabc",
+    linkedinLink: "https://www.linkedin.com/in/jessica-b-0b1b9b1b2/"
   },
 ];
