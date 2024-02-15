8cb1443edb4fe14cb980722b6947f61722230f55
devavaz <135300758+devavaz@users.noreply.github.com>
2024-02-16 00:28:19 +0900

    Team info updated

--- v47-client/src/components/landing/testimonials/Testimonials.jsx
+++ v47-client/src/components/landing/testimonials/Testimonials.jsx
@@ -1,8 +1,23 @@
+
+import React, { useEffect } from "react";
 import FeedbackCard from "./FeedbackCard";
 import { feedback } from "../../../lib/feedBackData";
+import Aos from "aos";
 
-const Testimonials = () => (
-  <section className="py-6 flex justify-center items-center flex-col relative ">
+
+const Testimonials = () => {
+
+  useEffect(() => {
+  Aos.init();
+}, []);
+
+  return(
+  <section   
+  className="py-6 flex justify-center items-center flex-col relative "
+      data-aos="fade-zoom-in"
+      data-aos-offset="200"
+      data-aos-easing="ease-in-sine"
+      data-aos-duration="600">
     <div className="w-full flex justify-end items-center flex-col sm:mb-16 mb-6 relative z-[1]">
       <h2 className="text-3xl lg:text-4xl text-gray-800 font-bold dark:text-gray-200 text-[#2d8630]">
         What People are saying about us
@@ -23,5 +38,5 @@ const Testimonials = () => (
       ))}
     </div>
   </section>
-);
+);}
 export default Testimonials;
