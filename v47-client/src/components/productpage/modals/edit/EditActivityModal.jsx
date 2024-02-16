12a8c6cdc09d2813996abfe99888b7bb9dc84d43
jess <120062852+jessabc@users.noreply.github.com>
2024-02-16 16:02:34 +1100

    modal on close; input logic

--- v47-client/src/components/productpage/modals/edit/EditActivityModal.jsx
+++ v47-client/src/components/productpage/modals/edit/EditActivityModal.jsx
@@ -14,12 +14,13 @@ function EditModal({
 }) {
 
   const { productData, setProductData } = useContext(productDataContext);
-
+ 
   const handleClick = () => {
     let updatedProductData = []
     let updatedCategory = {}
   
     productData.forEach(category => {
+      
       if(category.categoryName === categoryName) {
         const updatedActivtyTypes = category.activityTypes.map(activity => activity.activityName === activityName ? {...activity, activityName: editActivityNameInput} : activity)
         updatedCategory = {...category, activityTypes: updatedActivtyTypes}
@@ -34,17 +35,36 @@ function EditModal({
 
   return (
     <Portal>
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 bg-gray-800 z-50">
       <div className="bg-white p-8 rounded-lg w-96">
-        <InputFields
-          headerText={"Edit Activity"}
+        {/* <InputFields
+          headerText={"Edit Activity Name"}
           value={editActivityNameInput}
           onChangeValue={setEditActivityNameInput}
-        />
+        /> */}
+  
+        {/* originally InputFields resusable componenet above, was having trouble with the tolowercase.  */}
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-semibold mb-2"
+          >
+            Edit Activity 
+          </label>
+          <input
+            type="text"
+            className="w-full border  border-gray-300 px-3 py-2 rounded"
+            value={editActivityNameInput}
+            onChange={(e) => setEditActivityNameInput(e.target.value)}
+            required
+          />
+        </div>
 
         <Button
           onClick={handleClick}
-          onClose={() => setIsEditModalOpen(false)}
+          onClose={() => {
+            setIsEditModalOpen(false);
+            setEditActivityNameInput(activityName)
+          }}
           textValue={"Cancel"}
         />
       </div>
