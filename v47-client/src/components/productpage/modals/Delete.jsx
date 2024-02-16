5b2463b43419314413dc383869755adc27bf4d3f
jess <120062852+jessabc@users.noreply.github.com>
2024-02-16 15:58:41 +1100

    spacing

--- v47-client/src/components/productpage/modals/Delete.jsx
+++ v47-client/src/components/productpage/modals/Delete.jsx
@@ -1,4 +1,3 @@
-// Delete.jsx
 import React from 'react';
 import Portal from "./Portal/Portal"
 
@@ -6,8 +5,12 @@ export const Delete = ({ onDelete, onCancel, name }) => {
   return (
     <Portal>
     <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 bg-gray-800 z-50">
-      <div className="bg-white p-8 rounded-lg">
-      <p className="text-gray-700 text-lg mb-10 mx-20">{`Are you sure you want to delete '${name}'?`}</p>
+      <div className="bg-white p-8 rounded-lg w-96">
+        <div className="mb-8 px-3 py-2">
+          <p className="block text-gray-700 text-sm font-semibold text-center">
+            {`Are you sure you want to delete '${name}'?`}
+          </p>
+        </div>
         <div className="flex justify-evenly">
           <button
             className="bg-red-500 text-white px-8 py-2 mr-2 rounded"
