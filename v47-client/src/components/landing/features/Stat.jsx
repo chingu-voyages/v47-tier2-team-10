7a384faf6273c4316d21c9dc775737fa4213367d
jess <120062852+jessabc@users.noreply.github.com>
2024-02-18 19:43:50 +1100

    class to className, svg properties

--- v47-client/src/components/landing/features/Stat.jsx
+++ v47-client/src/components/landing/features/Stat.jsx
@@ -7,9 +7,9 @@ export default function Stat({stat}) {
 
     return (
         <div className='text-center'>
-            <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">{stat.title}</h4>
-            <p ref={ref} class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-teal-300 to-green-600">{count.toLocaleString()}+</p>
-            <p class="mt-1 text-gray-500">{stat.text}</p>
+            <h4 className="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">{stat.title}</h4>
+            <p ref={ref} className="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-teal-300 to-green-600">{count.toLocaleString()}+</p>
+            <p className="mt-1 text-gray-500">{stat.text}</p>
         </div>
     )
 }
